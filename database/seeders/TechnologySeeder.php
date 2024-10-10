217f43368a80c1779866b70f0e75faacf622fbda
Corfaya <lauracorrado0696@gmail.com>
2024-10-10 15:14:38 +0200

    [UPD] TechnologySeeder filled

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tech_arr = ['HTML', 'CSS', 'JS', 'EJS', 'Laravel', 'React', 'MySQL', 'Ruby'];
+
+        foreach($tech_arr as $tech) {
+            $new_tech = new Technology();
+            $new_tech->name = $tech;
+            $new_tech->slug = Technology::generateSlug($new_tech->name);
+
+            $new_tech->save();
+        }
     }
 }
